0bba04c1117092feb7c32b54e66413d316c59a9c
cj-nassif <nassif.cj@gmail.com>
2022-02-08 06:43:32 -0300

    Added splashScreen

--- App.tsx
+++ App.tsx
@@ -6,7 +6,12 @@ import { Home } from './src/pages/Home';
 
 
 
+
+
 export default function App() {
+
+
+
   return (
     <>
       <StatusBar
--- android/app/src/main/java/com/myskills/MainApplication.java
+++ android/app/src/main/java/com/myskills/MainApplication.java
@@ -20,20 +20,24 @@ public class MainApplication extends Application implements ReactApplication {
           return BuildConfig.DEBUG;
         }
 
+
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+
           return packages;
         }
+
+         
 
         @Override
         protected String getJSMainModuleName() {
           return "index";
         }
       };
+
 
   @Override
   public ReactNativeHost getReactNativeHost() {
